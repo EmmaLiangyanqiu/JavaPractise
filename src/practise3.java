1b0fe4a3698f57ef341870be2d52306b9bb6371f
EmmaLiangyanqiu <17865197939@163.com>
2018-08-04 13:56:50 +0800

    test

--- src/practise3.java
+++ src/practise3.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Emma on 2018/8/4.
@@ -9,7 +6,6 @@ import java.util.Map;
 public class practise3 {
     public static void main(String[] args) {
         Map<Integer, List<Integer>> map1 = new HashMap<Integer, List<Integer>>();
-        List<Integer> list_sort=new ArrayList<Integer>();
         List<Integer> list1 = new ArrayList<Integer>();
 
         System.out.println("随机生成50个小于100的数,分别为:" );
@@ -19,8 +15,39 @@ public class practise3 {
             System.out.print(num+",");
         }
 
+        for(int j=1;j<10;j++){
+            int key=j;
+            List<Integer> list_sort=new ArrayList<Integer>();
+            for(int i = 0 ; i < list1.size() ; i++) {
+                if(j==(int)list1.get(i)/10){
+                    list_sort.add(list1.get(i));
+                }
+            }
+            map1.put(key,list_sort);
+        }
+
         System.out.println();
-        System.out.println("Map中的数据为:");
+        System.out.print("Map中的数据为:");
+        for(Integer key:map1.keySet()){
+            List<Integer> list3=map1.get(key);
+            System.out.print(key+"=>"+list3+",");
+        }
+
+        for(Integer key:map1.keySet()){
+            List<Integer> list3=map1.get(key);
+            Collections.sort(list3);
+            map1.put(key,list3);
+        }
+        System.out.println();
+        System.out.print("排序后Map的数据为:");
+
+        for(Integer key:map1.keySet()){
+            List<Integer> list3=map1.get(key);
+            System.out.print(key+"=>"+list3+",");
+        }
+
+
+
 
 
 
